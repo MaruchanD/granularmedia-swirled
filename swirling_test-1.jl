8000b8c5ef92b482f09e4b0c34501a9a07782db2
MaruchanD_PC <archimedes0022@gmail.com>
2026-06-26 08:36:38 -0400

    Movimiento_Swirled_Test-1

---
+++ swirling_test-1.jl
@@ -0,0 +1,106 @@
+using StaticArrays
+using LinearAlgebra
+
+# DEFINICIÓN DE LA ESTRUCTURA DE LA PARTICULA
+mutable struct Particle{N, T}
+    r::SVector{N, T}      # Posición
+    v::SVector{N, T}      # Velocidad
+    a::SVector{N, T}      # Aceleración
+    mass::T               # Masa
+    radius::T             # Radio
+end
+
+# INTEGRADOR (VELOCITY-VERLET)
+function velocity_verlet_step!(particles::Vector{Particle{N, T}}, dt::T, calc_forces!) where {N, T}
+    for p in particles
+        p.r = p.r + p.v * dt + 0.5 * p.a * dt^2
+        p.v = p.v + 0.5 * p.a * dt
+    end
+    
+    calc_forces!(particles)
+    
+    for p in particles
+        p.v = p.v + 0.5 * p.a * dt
+    end
+end
+
+# FUERZAS APLICADAS A LAS PARTICULAS
+function contenedor_circular!(particles::Vector{Particle{N, T}}, radio_contenedor::T) where {N, T}
+    kn_wall = 1.0e5
+    gn_wall = 5.0e2
+
+    for p in particles
+        d = norm(p.r)
+        delta = d + p.radius - radio_contenedor
+        
+        if delta > 0.0
+            n_wall = -p.r / d
+            vn = dot(p.v, n_wall)
+            
+            Fn_mag = max(kn_wall * delta - gn_wall * vn, 0.0)
+            p.a += (Fn_mag * n_wall) / p.mass
+        end
+    end
+end
+
+function fuerza_inercial!(particles::Vector{Particle{N, T}}, omega::T) where {N, T}
+    for p in particles
+        # Fuerza centrífuga: F_c = m * omega^2 * r
+        F_c = p.mass * omega^2 * p.r
+        # Aceleración centrífuga: a_c = F_c / m = omega^2 * r
+        p.a += F_c / p.mass
+    end
+end
+
+function fuerza_total!(particles::Vector{Particle{N, T}}) where {N, T}
+    radio_contenedor = 5.0
+    omega = 2.0 * pi                                        # Velocidad angular del contenedor (rad/s)
+    contenedor_circular!(particles, radio_contenedor)       #Fuerza ejercida por el contenedor circular
+    fuerza_inercial!(particles, omega)                      #Fuerza centrífuga
+end
+
+# Exportacion a OVITO
+function guardar_frame_xyz!(archivo::String, particles::Vector{Particle{N, T}}, tiempo::Float64, radio_contenedor::T) where {N, T}
+    open(archivo, "a") do io
+        println(io, length(particles)+1)  # Número de partículas + contenedor
+        println(io, "Properties=species:S:1:pos:R:3:radius:R:1 Time=$tiempo")
+        for p in particles
+            println(io, "Granulo $(p.r[1]) $(p.r[2]) 0.0 $(p.radius)")
+        end
+        println(io, "Contenedor 0.0 0.0 0.0 $radio_contenedor")
+    end
+end
+
+# Ejecucion de prueba
+function simular_sistema()
+    sistema = [
+        #Particle(@SVector[0.0, 0.0], @SVector[0.0, 0.0], @SVector[0.0, 0.0], 1.0, 0.1),
+        Particle(@SVector[1.0, 1.0], @SVector[0.5, -0.5], @SVector[0.0, 0.0], 1.0, 0.5)#,
+        #Particle(@SVector[-1.5, 1.5], @SVector[-0.3, -0.2], @SVector[0.0, 0.0], 2.0, 0.2)
+    ]
+    # Parametros
+    radio_contenedor = 5.0
+    omega = 2.0 * pi # Velocidad angular del contenedor (rad/s)
+
+    dt = 1.0e-4
+    tiempo_total = 1.0 # 10.0 segundos de simulación física
+    pasos = round(Int, tiempo_total / dt)
+    frecuencia_guardado = 100 
+    
+    archivo_salida = "contenedor_swirling.xyz"
+    open(archivo_salida, "w") do io end # Limpiar archivo si existe
+
+    println("Iniciando movimiento swirled...")
+    
+    for paso in 1:pasos
+        velocity_verlet_step!(sistema, dt, fuerza_total!)
+        
+        if paso % frecuencia_guardado == 0
+            guardar_frame_xyz!(archivo_salida, sistema, paso * dt, radio_contenedor)
+        end
+    end
+    
+    println("¡Simulación terminada! Archivo generado: $archivo_salida")
+end
+
+simular_sistema()
